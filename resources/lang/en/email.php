009b10f9a1544eecdfdb31191721995b8242579c
MDavid <2527132+MDylan@users.noreply.github.com>
2022-03-05 21:57:15 +0100

    English translation

--- resources/lang/en/email.php
+++ resources/lang/en/email.php
@@ -1,12 +1,145 @@
 <?php
 
 return array (
+  'GroupUserLogout' => 
+  array (
+    'line_1' => 'We are informing you that you have left the :groupName group. You will no longer be able to see the calendar of the group. Also, your future services in this group have been deleted.',
+    'line_2' => 'Your group membership deletion has been initiated by :userName. If this is not you, you shall contact him to discuss the reasons.',
+    'subject' => 'You have left the :groupName group.',
+  ),
+  'ParentGroupAttached' => 
+  array (
+    'line_1' => 'We are informing you that a new sub-group has been assigned in your :groupName group.',
+    'line_2' => 'From now on, all publishers will be copied automatically into the sub-group.',
+    'line_3' => 'Sub-group name: :childGroupName',
+    'line_4' => 'The modification has been performed by :userName.',
+    'line_5' => 'If you wish to change this setting, please go to the "Publishers" menu and detach the link.',
+    'subject' => 'Important: A new main group has been set.',
+  ),
+  'ParentGroupDetached' => 
+  array (
+    'line_1' => 'We are informing you that your :childGroupName group has been detached from the foregoing main group.',
+    'line_2' => 'Publishers will be not be copied automatically into this group any more.',
+    'line_3' => 'The name of the foregoing main group: :groupName',
+    'line_4' => 'The modification has been performed by :userName.',
+    'line_5' => 'If you wish to modify this setting, please go to the "Publishers" menu to reattach the groups.',
+    'subject' => 'Important: The main group setting has been detached.',
+  ),
+  'dear' => 'Dear',
+  'deletePersonalData' => 
+  array (
+    'line_1' => 'You have requested us to delete your personal data.',
+    'line_2' => 'Please confirm your request by clicking on the link below. By clicking on the button, deletion will be performed automatically.',
+    'line_3' => 'For safety reasons, this link remains valid for 1 hour. If you are not the one who requested the deletion of your personal data, do not click on the link. Change your password immediately, because apparently somebody else has used your account.',
+    'subject' => 'Deleting personal data',
+  ),
   'event' => 
   array (
+    'created' => 
+    array (
+      'line_1' => 'We are informing you that :userName has scheduled the following service for you in the :groupName group.',
+      'line_2' => ':newServiceDate',
+      'line_3' => 'Enjoy your service! :)',
+      'subject' => 'Your planned service on :date (:groupName)',
+    ),
     'deleted' => 
     array (
-      'line_1' => 'We would like to inform you, that your service has been deleted in :groupName group:',
-      'subject' => 'Your service at :date has been deleted! (:groupName)',
+      'line_1' => 'We are informing you that your following service has been deleted in the :groupName group:',
+      'line_2' => ':oldServiceDate',
+      'line_3' => 'Reason for deletion: :reason',
+      'line_4' => 'Deletion has been initiated by :userName. He can provide further information.',
+      'subject' => 'Your service on :date has been deleted! (:groupName)',
+    ),
+    'deleted_to_admin' => 
+    array (
+      'line_1' => 'We are informing you that the following service in the :groupName group has been deleted:',
+      'line_2' => ':oldServiceDate',
+      'line_3' => 'Reason for deletion: :reason',
+      'line_4' => 'Deletion has been initiated by :userName. He can provide further information.',
+      'subject' => 'Service on :date has been deleted! (:groupName)',
+    ),
+    'deletion_reasons' => 
+    array (
+      'modified_service_time' => 'The time of the service on this day has been modified, and your planned service did not fit into the new service time.',
+      'service_day_deleted' => 'No further services can be scheduled for this day, so the service of all members have been deleted.',
+      'unknown' => 'Unknown.',
+      'user_logout' => 'You have left the group, and so your future services in the group have been deleted.',
+    ),
+    'modified' => 
+    array (
+      'line_1' => 'We are informing you that your following service in the :groupName group has been modified:',
+      'line_2' => 'Original service date: :oldServiceDate',
+      'line_3' => 'New service date: :newServiceDate',
+      'line_4' => 'Please plan your service accordingly.',
+      'line_5' => 'Reason for modification: :reason',
+      'line_6' => 'Modification has been initiated by :userName. He can provide further information.',
+      'subject' => 'Your service scheduled for :date has been modified. (:groupName)',
+    ),
+    'modify_reasons' => 
+    array (
+      'modified_service_time' => 'The time of the service on this day has been modified, and your planned service did not fit into the new service time.',
+      'unknown' => 'Unknown',
+    ),
+    'status_changed' => 
+    array (
+      0 => 
+      array (
+        'line_1' => 'We are informing you that your following service in the :groupName group is in the approval process. One of the group servants will soon decide if the time/date is feasible. You will be notified about the decision. Until his feedback arrives, please do not consider the scheduled service as fixed.',
+        'line_2' => ':newServiceDate',
+        'subject' => 'Your service is in the approval process :date',
+      ),
+      1 => 
+      array (
+        'line_1' => 'We are informing you that the following service in the :groupName group has been approved. Enjoy your service! :)',
+        'line_2' => ':newServiceDate',
+        'subject' => 'Your scheduled service has been approved :date!',
+      ),
+      2 => 
+      array (
+        'line_1' => 'We are informing you that the following service in the :groupName group has been declined. Please choose an other time/date.',
+        'line_2' => ':newServiceDate',
+        'subject' => 'The scheduled service has been declined.',
+      ),
     ),
   ),
+  'finishRegistration' => 
+  array (
+    'done' => 
+    array (
+      'line_1' => 'Welcome as one of the users of this page! Now you can log in using your previously set password. You can accept or decline incoming group invitations in the "Groups" menu. For further information on how to use the page, please visit the "Help" menu.',
+      'subject' => 'Registration successfully finished!',
+    ),
+    'line_1' => 'We are informing you that :groupAdmin has invited you to become the user of the :appName page.',
+    'line_2' => 'Your registration has not become valid yet. You have :day days to confirm your registration by clicking on the link below. In absence of your confirmation, your account will be deleted automatically.',
+    'line_3' => 'Username: :userMail',
+    'line_4' => 'During the registration confirmation process, you will be able to set your new password.',
+    'line_5' => 'The link below can be used only for confirming your registration.',
+    'subject' => 'You have been invited to become the user of the :appName page.',
+  ),
+  'groupUserAdded' => 
+  array (
+    'line_1' => 'This is an automatically generated notification about :groupAdmin inviting you to join the :groupName group.',
+    'line_2' => 'After login, you shall accept or decline this invitation in the "Groups" menu.',
+    'subject' => 'Invitation to the :groupName group',
+  ),
+  'loginData' => 
+  array (
+    'line_1' => ':groupAdmin has created an account for you on the page.',
+    'line_2' => 'Login data:',
+    'line_3' => 'Username: :userMail',
+    'line_4' => 'Password: :userPassword',
+    'line_5' => 'After login, please make sure to change this password under the "My profile" menu.',
+    'subject' => 'Login data',
+  ),
+  'newadmin' => 
+  array (
+    'line_1' => 'This is an automatically generated notification about :newAdmin being assigned to be the administrator of your page.',
+    'line_2' => 'Assigned by: :adminBy',
+    'line_3' => 'If this proves to be a mistake, please revoke his Administrator access permission as soon as possible.',
+    'subject' => 'A new administrator has been assigned.',
+  ),
+  'registerFail' => 'If you are not the one who performed the registration, it will be deleted automatically after 48 hours.',
+  'registerWelcome' => 'Successful registration on :url. Please click on the link below to confirm your registration.',
+  'signature' => 'Your brothers, 
+The authors of the Help Service Organizer page',
 );
---
+++ resources/lang/en/event.php
@@ -0,0 +1,83 @@
+<?php
+
+return array (
+  'approval_info' => 'Approval',
+  'approved' => 'Approved',
+  'cancel_edit' => 'Cancel modification(s)',
+  'choose_group' => 'Choose a group',
+  'choose_publisher' => 'Choose a publisher',
+  'choose_time' => 'Choose a date and time',
+  'comment' => 
+  array (
+    'helper' => 'Remarks for your service. Can be seen by every member. Please compose briefly.',
+    'label' => 'Remarks',
+  ),
+  'confirmDelete' => 
+  array (
+    'error' => 'Error while deleting',
+    'message' => 'This cannot be undone.',
+    'question' => 'Are you sure you want to delete the service?',
+    'success' => 'Service successfully deleted.',
+  ),
+  'create_event' => 'Choose a date and time for your service',
+  'date' => 'Date',
+  'delete_event' => 'Delete service',
+  'edit_event' => 'Edit service',
+  'error' => 
+  array (
+    'invalid_group' => 'Invalid group or lack of editing access permission.',
+    'no_permission' => 'You do not have access permission.',
+    'no_service_day' => 'This day cannot be chosen for service.',
+    'publisher_busy' => 'You have already signed up for service in this time slot in an other group. (:start - :end)',
+  ),
+  'event_time' => 'Service time period',
+  'eventsBar' => 
+  array (
+    'no_events' => 'You do not have any upcoming services.',
+    'title' => 'Upcoming services',
+  ),
+  'invalid_value' => 'Invalid time value.',
+  'modal' => 
+  array (
+    'tab_events' => 'Date and time',
+    'tab_set_event' => 'Schedule date and time',
+  ),
+  'need_approval' => 'Important: The scheduled service must be approved by the group servant.',
+  'no_last_events' => 'You have not yet performed service in this month.',
+  'paralel_events' => 'In the chosen time slot, you have already signed up for service in the following group. If this application is approved, the other one will be deleted automatically - and vice versa.',
+  'please_wait' => 'Please wait...',
+  'publisher' => 'Publisher',
+  'reach_max_publisher' => 'In this time slot, the max. number of publishers has already been reached. Choose an other time slot.',
+  'save' => 'Save appointment',
+  'save_changes' => 'Save modification(s)',
+  'saved' => 'Service saved',
+  'service' => 
+  array (
+    'bible_studies' => 'Bible studies started',
+    'error' => 'You cannot record publication distributions for this event.',
+    'language' => 'Language',
+    'no_placements' => 'No publications distributed',
+    'note' => 'Remarks',
+    'placements' => 'Publications distributed',
+    'return_visits' => 'Return visits',
+    'success' => 'Publication distributions have been saved.',
+    'videos' => 'Videos',
+  ),
+  'service_end' => 'End of service',
+  'service_start' => 'Start of service',
+  'status' => 'Status',
+  'status_0' => 'Waiting for approval',
+  'status_1' => 'Approved',
+  'status_2' => 'Declined',
+  'switch' => 'Switch',
+  'weekdays_short' => 
+  array (
+    0 => 'Sun',
+    1 => 'Mon',
+    2 => 'Tue',
+    3 => 'Wed',
+    4 => 'Thu',
+    5 => 'Fri',
+    6 => 'Sat',
+  ),
+);
