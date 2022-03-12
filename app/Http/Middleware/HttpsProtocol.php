cd62da1b1839aa12855592d33300fc4305907774
MDavid <2527132+MDylan@users.noreply.github.com>
2022-03-12 23:47:48 +0100

    Fix https redirection Fix header view

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,7 +16,7 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && app()->environment('production') && env('USE_HTTPS', false)) {
+        if (!$request->secure() && app()->environment('production') && env('USE_HTTPS', "false") == "true") {
             return redirect()->secure($request->getRequestUri());
         }
 
--- resources/views/livewire/groups/list-users.blade.php
+++ resources/views/livewire/groups/list-users.blade.php
@@ -3,10 +3,10 @@
     <div class="content-header">
         <div class="container-fluid">
         <div class="row mb-2">
-            <div class="col-sm-6">
+            <div class="col-sm-8">
             <h1 class="m-0">{{ __('group.users') }} ({{$group_name}})</h1>
             </div><!-- /.col -->
-            <div class="col-sm-6">
+            <div class="col-sm-4">
             <ol class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="{{route('home.home')}}">{{ __('app.menu-home') }}</a></li>
                 <li class="breadcrumb-item"><a href="{{route('groups')}}">{{ __('app.menu-groups') }}</a></li>
