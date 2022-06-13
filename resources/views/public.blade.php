eee1e28f763ac09729fa8cccb729c43619b6dd6d
MDavid <2527132+MDylan@users.noreply.github.com>
2022-06-13 11:53:33 +0200

    Change favicon

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" type="image/jpg" href="{{ asset('favicon.png') }}"/>
+  <link rel="shortcut icon" type="image/jpg" href="{{ asset('pmo-favicon.png') }}"/>
   <title>@lang('app.title') | @yield('title')</title>
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
