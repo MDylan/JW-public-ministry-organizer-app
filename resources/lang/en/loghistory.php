60efe2871dcfb12f3639612094d72310ffebbd63
MDavid <2527132+MDylan@users.noreply.github.com>
2022-11-07 13:47:51 +0100

    Engish translation

---
+++ resources/lang/en/loghistory.php
@@ -0,0 +1,29 @@
+<?php
+
+return array (
+  'change_type' => 
+  array (
+    'new' => 'new',
+    'old' => 'old',
+  ),
+  'events' => 
+  array (
+    'created' => ':UserName created at :date',
+    'deleted' => ':UserName deleted this, at :date',
+    'updated' => ':UserName modified at :date',
+  ),
+  'history_button' => 'Changes',
+  'history_title' => 'Recent changes',
+  'models' => 
+  array (
+    'Event' => 
+    array (
+      'accepted_at' => 'Approved',
+      'accepted_by' => 'Approved by',
+      'comment' => 'Comment',
+      'end' => 'Service end',
+      'start' => 'Service start',
+      'status' => 'Status',
+    ),
+  ),
+);
