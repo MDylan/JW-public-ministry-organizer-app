0384f81ff4d81d380f6d37e391183a794ff72501
MDavid <2527132+MDylan@users.noreply.github.com>
2022-11-28 13:24:57 +0100

    New: Dialy users in statistics

--- resources/views/livewire/admin/statistics.blade.php
+++ resources/views/livewire/admin/statistics.blade.php
@@ -29,9 +29,18 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
-                    <canvas id="myChart" width="100" height="30"></canvas>
+                <div class="col-md-6">
+                    <div class="card card-primary card-outline">
+                        <div class="card-body">
+                            <canvas id="active_users" width="100" height="50"></canvas>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-md-6">
+                    <div class="card card-primary card-outline">
+                        <canvas id="dialy_users" width="100" height="50"></canvas>
+                    </div>
                 </div>
                 
             </div>
@@ -43,38 +52,60 @@
     @section('footer_scripts')
         <script src="{{ asset('plugins/chart.js/Chart.bundle.min.js') }}"></script>
         <script>
-            var ctx = document.getElementById('myChart').getContext('2d');
+            var ctx = document.getElementById('active_users').getContext('2d');
             var myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: [
-                        @foreach ($periods as $period)
-                            '{{ $period->format(__('app.format.time')) }}',
+                        @foreach ($active_users_array as $period => $number)
+                            '{{ \Carbon\Carbon::parse($period)->format(__('app.format.time')) }}',
                         @endforeach
                     ],
                     datasets: [{
                         label: '@lang('user.online')',
                         data: [
-                            @foreach ($stats as $stat)
-                                {{ $stat['number'] }},
+                            @foreach ($active_users_array as $number)
+                                {{ $number }},
+                            @endforeach
+                        ],
+                        backgroundColor: 
+                            'rgba(255, 99, 132, 0.2)',
+                        borderColor: 
+                            'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+
+            var ctx = document.getElementById('dialy_users').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: [
+                        @foreach ($dialy_users_array as $period => $number)
+                            '{{ $period }}',
+                        @endforeach
+                    ],
+                    datasets: [{
+                        label: '@lang('user.online')',
+                        data: [
+                            @foreach ($dialy_users_array as $number)
+                                {{ $number }},
                             @endforeach
                         ],
                         backgroundColor: //[
-                            'rgba(255, 99, 132, 0.2)',
-                        //     'rgba(54, 162, 235, 0.2)',
-                        //     'rgba(255, 206, 86, 0.2)',
-                        //     'rgba(75, 192, 192, 0.2)',
-                        //     'rgba(153, 102, 255, 0.2)',
-                        //     'rgba(255, 159, 64, 0.2)'
-                        // ],
+                            'rgb(122, 148, 255, 0.5)',
                         borderColor: //[
-                            'rgba(255, 99, 132, 1)',
-                        //     'rgba(54, 162, 235, 1)',
-                        //     'rgba(255, 206, 86, 1)',
-                        //     'rgba(75, 192, 192, 1)',
-                        //     'rgba(153, 102, 255, 1)',
-                        //     'rgba(255, 159, 64, 1)'
-                        // ],
+                            'rgb(122, 148, 255, 1)',
                         borderWidth: 1
                     }]
                 },
