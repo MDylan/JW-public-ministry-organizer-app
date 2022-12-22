0e6f7ba7d8cfdbdb9ea0da5fc9ec6e274b2f7fef
MDavid <2527132+MDylan@users.noreply.github.com>
2022-12-22 20:10:48 +0100

    Removed unused string

--- resources/lang/hu/email.php
+++ resources/lang/hu/email.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'dear' => 'Kedves',
     'registerWelcome' => 'Elkészült a regisztrációd a :url oldalon. Kérjük, hogy aktiváld a regisztrációdat azzal, hogy az alábbi hivatkozásra kattintasz.',
     'registerFail' => 'Amennyiben nem te kérted ezt a regisztrációt, regisztrációdat automatikusan törölni fogjuk 48 óra múlva.',
     'signature' => 'Üdvözlettel a Segélyszolgálat Szervező oldal készítői',
