a575a89b1eb0fdf84bb11654986656f2986e8311
MDavid <2527132+MDylan@users.noreply.github.com>
2026-03-18 14:48:35 +0100

    no ssl validation on local server

--- config/mail.php
+++ config/mail.php
@@ -43,6 +43,13 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
+            'stream' => env('APP_ENV') == "local" ? [
+                'ssl' => [
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true,
+                ],
+            ] : [],
         ],
 
         'phpmail' => [
